27d4dc8f1de1d2a7018fffa23986f4968796058a
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2022-11-04 13:56:18 +0000

    Sets absolute path for local links to pages & img

--- src/modifyFileContents.ts
+++ src/modifyFileContents.ts
@@ -1,29 +1,33 @@
-import {FileDetails} from "./getAllFiles";
-import {IFileContentOptions} from "spsave";
-import {extname} from "path";
+import { FileDetails } from "./getAllFiles";
+import { IFileContentOptions } from "spsave";
+import { extname } from "path";
+import {Config} from "./config";
 
-export function modifyFileContents(fileDetails: FileDetails[], destinationPath: string) : IFileContentOptions[] {
+export function modifyFileContents(fileDetails: FileDetails[], config: Config): IFileContentOptions[] {
     const filesToUpload: IFileContentOptions[] = [];
+    const absoluteUrl = config.siteUrl + "/" + config.destinationPath + "/"
     fileDetails.forEach((fileDetail) => {
         if (extname(fileDetail.name) != ".md") {
             console.log(`${fileDetail.name} is not a markdown file so skipping`);
             filesToUpload.push({
                 fileName: fileDetail.name,
                 fileContent: fileDetail.buffer,
-                folder: destinationPath
+                folder: config.destinationPath
             });
-            return;
+        } else {
+
+            let fileAsString = fileDetail.buffer.toString();
+
+            // Modify the file contents here!!!!
+            // modify absoulte path to resource images and link
+            fileAsString = fileAsString.replaceAll("\]\((?!http)","](" + absoluteUrl)
+
+            filesToUpload.push({
+                fileName: fileDetail.name,
+                fileContent: fileAsString,
+                folder: config.destinationPath
+            })
         }
-
-        const fileAsString = fileDetail.buffer.toString();
-
-        // Modify the file contents here!!!!
-
-        filesToUpload.push({
-            fileName: fileDetail.name,
-            fileContent: fileAsString,
-            folder: destinationPath
-        })
     });
     return filesToUpload;
 }
